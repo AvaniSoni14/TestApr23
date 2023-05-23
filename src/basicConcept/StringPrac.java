cfba701f87ce97f02a2d7976abe4539f3e85c48a
Avani Soni <avanisoni140698@gmail.com>
2023-05-23 08:50:33 +0530

    creating_conflict

--- src/basicConcept/StringPrac.java
+++ src/basicConcept/StringPrac.java
@@ -3,7 +3,8 @@ package basicConcept;
 public class StringPrac {
 	
 	public static void main(String[] args) {
-		System.out.println("I am in main method..of branch A");
-		System.out.println("Conflict resolved");
+		System.out.println("I am in main method..of branch A2");
+		System.out.println("Crecting conflict....");
+		System.out.println("hi.....");
 	}
 }
