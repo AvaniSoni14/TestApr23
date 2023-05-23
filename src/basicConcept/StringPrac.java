a2d3eb0016b59e2901f72c32c8ce4710058b357c
Avani Soni <avanisoni140698@gmail.com>
2023-05-23 08:58:04 +0530

    branch B chnages

--- src/basicConcept/StringPrac.java
+++ src/basicConcept/StringPrac.java
@@ -5,5 +5,6 @@ public class StringPrac {
 	public static void main(String[] args) {
 		System.out.println("I am in main method..of branch A");
 		System.out.println("Conflict resolved");
+		System.out.println("hello.....");
 	}
 }
